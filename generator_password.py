7c5b43c8d4fd7bcad35cc7a29ffbf751b449412a
Ярослав <asssv0423348@gmail.com>
2025-03-18 20:51:59 +0300

    6 сохранение

--- generator_password.py
+++ generator_password.py
@@ -18,17 +18,22 @@ class Password:
                 strok_password = random.choice(alfavit)
                 number_password = random.choice(numbers)
                 strok_password2 = random.choice(alfavit_simvol)
-                password = password + strok_password + number_password + strok_password2
+                password += strok_password + number_password + strok_password2
             
-            if os.path.getsize("C:/Users/admin/password_d/password.txt") == 0:
-                with open("C:/Users/admin/password_d/password.txt", "w") as f:
-                    f.write(f" {name_ak} пароль: {password}")
-                    return True
-            else:
-                with open("C:/Users/admin/password_d/password.txt", "a") as f:
-                    f.write(f"1 {name_ak} пароль: {password}" + '\n')
-                    return False
+            with open(f"C:/Users/admin/password_d/password_{name_ak}.txt", "w") as f:
+                f.write(f" {name_ak} пароль: {password}" + '\n')
+                return True
                 
+        except Exception as e:
+            return e
+        
+    def see_password(self, name_ak):
+        try:
+            with open(f"C:/Users/admin/password_d/password_{name_ak}.txt", "r") as f:
+                result = f.readlines()
+                for line in result:
+                    print(line)
+
         except Exception as e:
             return e
 
@@ -44,19 +49,21 @@ def main():
             print("Выберите ниже цифру и напишите её для выполнение команды: ")
             print("0: завершить программу")
             print("1: сгенерировать новый пароль и сохранить его")
+            print("2 : просмотреть пароль ак")
             make_user = int(input())
             if make_user == 1:
                 user = input("Введите ак, для которого вы генерируете пароль: ")
                 len_p = int(input("Введите длину пароля: "))
                 result = generat_password.generated_password(user, len_p)
+                print("Задача выполнена")
             
             if make_user == 0:
                 break
-
-            if result:
-                print("Задача выполнена")
-            else:
-                print("Что-то пошло не так")
+            
+            if make_user == 2:
+                name_ak = input("Введите ак для просмотра его пароля: ")
+                generat_password.see_password(name_ak)
+                print("Задача Выполнена")
                 
             
         except Exception as e:
